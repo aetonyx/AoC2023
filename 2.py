2993c2e5ae5fe6c83c311993be1a84399bd468c0
Aetonyx <aetonyx@gmail.com>
2023-12-01 23:11:36 -0800

    Day 2

---
+++ 2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+gameCounter = 1 # game counter
+validGames = 0
+powerGames = 0
+
+# 12 red cubes, 13 green cubes, and 14 blue cubes
+red_max = 12
+green_max = 13
+blue_max = 14
+
+#line = "Game 12: 1 green, 8 red, 5 blue; 6 red, 12 blue; 2 blue, 15 red; 14 blue, 15 red, 1 green; 8 red, 9 blue"
+
+def isGamePossible(gameLine):
+    gameDraws = gameLine.split(': ')[1]
+    for draw in gameDraws.split('; '):
+        cubes = draw.split(", ")
+        for color in cubes:
+            a = color.split(" ")
+            if a[1] == "green" and int(a[0]) > green_max: return False
+            if a[1] == "red" and int(a[0]) > red_max: return False
+            if a[1] == "blue" and int(a[0]) > blue_max: return False
+    return True
+
+def getPowerSet(gameLine):
+    bluePower = 1
+    greenPower = 1
+    redPower = 1
+    gameDraws = gameLine.strip().split(': ')[1]
+    for draw in gameDraws.split('; '):
+        cubes = draw.split(", ")
+        for color in cubes:
+            a = color.split(" ")
+            if a[1] == "green": greenPower = max(greenPower, int(a[0]))
+            if a[1] == "blue": bluePower = max(bluePower, int(a[0]))
+            if a[1] == "red": redPower = max(redPower, int(a[0]))
+    return bluePower * greenPower * redPower
+
+# first part
+with open("input_2.txt") as gamesFile:
+    for game in gamesFile:
+        if isGamePossible(game):
+            #print(game)
+            validGames += gameCounter
+        gameCounter += 1
+
+# second part
+with open("input_2.txt") as gamesFile:
+    for game in gamesFile:
+        powerGames += getPowerSet(game)
+        
+print('Sum of valid games: ' + str(validGames))
+print('Sum of power games: ' + str(powerGames))
